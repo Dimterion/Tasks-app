c20cbe742c90cb68bf5f88307b0e5f7ec49ec1eb
Dimterion <dimterion59@gmail.com>
2022-11-07 16:59:50 +0100

    Added rating selection.

--- src/components/TaskForm.jsx
+++ src/components/TaskForm.jsx
@@ -1,17 +1,34 @@
 import { useState } from "react";
+import RatingSelect from "./RatingSelect";
 import Card from "./shared/Card";
+import Button from "./shared/Button";
 
 function TaskForm() {
   const [text, setText] = useState("");
+  const [rating, setRating] = useState(10);
+  const [btnDisabled, setBtnDisabled] = useState(true);
+  const [message, setMessage] = useState("");
 
-  const handleTextChange = (e) => {
-    setText(e.target.value);
+  const handleTextChange = ({ target: { value } }) => {
+    if (value === "") {
+      setBtnDisabled(true);
+      setMessage(null);
+    } else if (value.trim().length < 5) {
+      setBtnDisabled(true);
+      setMessage("Task must be at least 5 characters long.");
+    } else {
+      setMessage(null);
+      setBtnDisabled(false);
+    }
+
+    setText(value);
   };
 
   return (
     <Card>
       <form>
         <h2>Add new task</h2>
+        <RatingSelect select={setRating} selected={rating} />
         <div className="input-group">
           <input
             onChange={handleTextChange}
@@ -19,8 +36,11 @@ function TaskForm() {
             placeholder="Type your task"
             value={text}
           />
-          <button type="submit">Add</button>
+          <Button type="submit" isDisabled={btnDisabled}>
+            Add
+          </Button>
         </div>
+        {message && <div className="message">{message}</div>}
       </form>
     </Card>
   );
